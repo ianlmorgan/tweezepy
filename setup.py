e2dde991acd2814471f1a638ae9a15fcc270dee3
Ian L Morgan <ilmorgan@ucsb.edu>
2020-05-05 16:40:23 -0700

    Added setup.py
    Adding setup.py so I can try it out as a package

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  5 16:31:29 2020
+
+Author: Ian L. Morgan
+email: ilmorgan@ucsb.edu
+"""
+
+from setuptools import setup
+
+setup(
+   name='tweezepy',
+   version='0.1.0',
+   author='An Awesome Coder',
+   author_email='ilmorgan@ucsb.edu',
+   packages=['tweezepy'],
+   license='LICENSE.txt',
+   description='Saleh lab tweezer code',
+   long_description=open('README.md').read(),
+   install_requires=[
+       "numpy",
+       "pandas"
+       ],
+)
