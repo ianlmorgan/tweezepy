8cc713c9819634397dd2eaf0ed8177d4461032b1
Ian L Morgan <ilmorgan@ucsb.edu>
2020-06-30 14:12:59 -0700

    Added matplotlib dependency

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
    description='Saleh lab tweezer code',
    long_description=open('README.md').read(),
    install_requires=[
+       "matplotlib",
        "numpy",
        "pandas",
        "scipy",
