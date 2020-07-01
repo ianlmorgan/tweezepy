17a380783bb9147db149de96e32732481f0cc96c
Ian L Morgan <ilmorgan@ucsb.edu>
2020-06-30 17:08:53 -0700

    Fixing long description.

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ email: ilmorgan@ucsb.edu
 import pathlib
 import setuptools
 # The directory containing this file
-HERE = pathlib.Path(__file__).parent
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
@@ -16,12 +19,13 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setuptools.setup(
    name='tweezepy',
-   version='0.1.2',
+   version='0.1.3',
    author='Ian Morgan',
    author_email='ilmorgan@ucsb.edu',
    packages=setuptools.find_packages(),
    license='LICENSE.txt',
    description='Single-molecule pulling analysis package',
+   long_description=long_description,
    long_description_content_type='text/markdown',
    url='https://github.com/pypa/tweezepy',
    install_requires = ['autograd'],
