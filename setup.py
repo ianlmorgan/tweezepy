61e451b58197797e5ae54574bd6bfee363711d5e
Ian L Morgan <ilmorgan@ucsb.edu>
2020-07-12 09:49:57 -0700

    Updating version so it gets put on pypi

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 # This call to setup() does all the work
 setuptools.setup(
    name='tweezepy',
-   version='0.1.4',
+   version='0.1.5',
    author='Ian Morgan',
    author_email='ilmorgan@ucsb.edu',
    packages=setuptools.find_packages(),
