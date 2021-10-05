03cae21470b27e91689411cb73bab35960633cd8
Ian L Morgan <ilmorgan@ucsb.edu>
2021-10-05 15:01:47 -0700

    Adjusting docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,8 @@ release = '1.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,9 +47,20 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
+html_title = 'Tweezepy'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_theme_options = {
+    "path_to_docs": "docs",
+    "repository_url": "https://github.com/ianlmorgan/tweezepy",
+    "repository_branch": "main",
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    "use_repository_button": True,
+    "use_download_button": True,
+}
+html_baseurl = "https://tweezepy.readthedocs.io/en/latest/"
---
+++ docs/source/index.md
@@ -0,0 +1,21 @@
+.. Tweezepy documentation master file, created by
+   sphinx-quickstart on Tue Aug 10 10:31:37 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Tweezepy's documentation!
+====================================
+
+## Documentation
+
+```{toctree}:maxdepth: 2
+
+install
+pages/quickstart
+pages/
+api
+```
+
+## Authors & License
+
+Built by [Ian L. Morgan](https://github.com/ianlmorgan). Licensed under the GNU General Public License (see `LICENSE`).
