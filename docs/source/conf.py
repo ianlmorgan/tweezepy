7e400ccb727f4959c1e3779a4f156b24c262c94b
Ian L Morgan <ilmorgan@ucsb.edu>
2021-10-07 15:59:45 -0700

    adjust readthedocs.yml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ master_doc = "index"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-# exclude_patterns = ["_build"]
+exclude_patterns = ["_build"]
 
 
 # -- Options for HTML output -------------------------------------------------
