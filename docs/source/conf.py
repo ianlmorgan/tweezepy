0d5cdb8c089e3b72a885b0db0e268331fcb48b28
Ian L Morgan <ilmorgan@ucsb.edu>
2021-10-07 15:48:33 -0700

    adjusting conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,7 @@ extensions = ["sphinx.ext.autodoc",
 
 #source_suffix = {
 #                ".rst":"restructuredtext",
-#                ".ipynb":"myst-nb",
+#                ".ipynb":"nbsphinx",
 #}
 master_doc = "index"
 
@@ -50,7 +50,7 @@ master_doc = "index"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build"]
+# exclude_patterns = ["_build"]
 
 
 # -- Options for HTML output -------------------------------------------------
