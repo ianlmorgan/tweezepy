0e8e52a165e48479782d5fec261445438246ed1e
Ian L Morgan <ilmorgan@ucsb.edu>
2021-10-08 10:16:02 -0700

    cleaning up json from __init__.py

--- tweezepy/__init__.py
+++ tweezepy/__init__.py
@@ -3,15 +3,22 @@ from .smmcalibration import PSD
 from .simulations import simulate_trace
 from .simulations import downsampled_trace
 
-import json
+
 import os
 
-this_directory = os.path.abspath(os.path.dirname(__file__))
-pkginfo_path = os.path.join(this_directory,
-                            'tweezepy_info.json')
-pkginfo = json.load(open(pkginfo_path))
+from pkg_resources import DistributionNotFound,get_distribution
 
-__version__ = pkginfo["version"]
+try:
+    __version__ = get_distribution("tweezepy").version
+except DistributionNotFound:
+    __version__ = "unknown version"
+
+#this_directory = os.path.abspath(os.path.dirname(__file__))
+#pkginfo_path = os.path.join(this_directory,
+#                            'tweezepy_info.json')
+#pkginfo = json.load(open(pkginfo_path))
+
+#__version__ = pkginfo["version"]
 __all__ = [
            "AV",
            "PSD",
