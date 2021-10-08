93007c0c1dd332abde303cd01f0d5b42676cba0d
Ian L Morgan <ilmorgan@ucsb.edu>
2021-10-08 09:54:42 -0700

    remove json requirements

--- setup.py
+++ setup.py
@@ -6,33 +6,58 @@ Author: Ian L. Morgan
 email: ilmorgan@ucsb.edu
 """
 import setuptools
-import json
-import os
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+#import json
+#import os
+#this_directory = os.path.abspath(os.path.dirname(__file__))
+#with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+#    long_description = f.read()
 
-pkginfo_path = os.path.join(this_directory,
-                            'tweezepy',
-                            'tweezepy_info.json')
-pkginfo = json.load(open(pkginfo_path))
+NAME = "Tweezepy"
+VERSION = "1.0.4"
+DESCRIPTION = "Single-molecule force spectroscopy calibration"
+AUTHOR = "Ian L. Morgan"
+AUTHOR_EMAIL = "ilmorgan@ucsb.edu"
+URL = "https://github.com/ianlmorgan/tweezepy"
+LICENSE = "LGPLv3+"
+
+#pkginfo_path = os.path.join(this_directory,
+#                            'tweezepy',
+#                            'tweezepy_info.json')
+#pkginfo = json.load(open(pkginfo_path))
 
 # This call to setup() does all the work
-setuptools.setup(name=pkginfo['name'],
-                 version=pkginfo['version'],
-                 description=pkginfo['description'],
-                 author=pkginfo['author'],
-                 author_email=pkginfo['author_email'],
-                 url=pkginfo['url'],
-                 license=pkginfo['license'],
+setuptools.setup(name=NAME,
+                 version=VERSION,
+                 description=DESCRIPTION,
+                 author=AUTHOR,
+                 author_email=AUTHOR_EMAIL,
+                 url=URL,
+                 license=LICENSE,
                  packages=setuptools.find_packages(),
                  long_description=long_description,
                  long_description_content_type = 'text/markdown',
-                 install_requires = ['numba',
-                                     'numpy',
-                                     'scipy',
-                                     'autograd',
-                                     'emcee'],
+                 install_requires = ['numpy'],
+                 setup_requires = ["setuptools>=40.6.0"],
                  tests_require=['unittest',
                                 'numpy'],
                  ) 
+
+# This call to setup() does all the work
+#setuptools.setup(name=pkginfo['name'],
+#                 version=pkginfo['version'],
+#                 description=pkginfo['description'],
+#                 author=pkginfo['author'],
+#                 author_email=pkginfo['author_email'],
+#                 url=pkginfo['url'],
+#                 license=pkginfo['license'],
+#                 packages=setuptools.find_packages(),
+#                 long_description=long_description,
+#                 long_description_content_type = 'text/markdown',
+#                 install_requires = ['numba',
+#                                     'numpy',
+#                                     'scipy',
+#                                     'autograd',
+#                                     'emcee'],
+#                 tests_require=['unittest',
+#                                'numpy'],
+#                 ) 
