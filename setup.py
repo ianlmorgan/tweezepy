150742a15e943389b2e38c1d39303a05f8a361e1
Ian L Morgan <ilmorgan@ucsb.edu>
2021-10-08 09:57:49 -0700

    add in long description

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ email: ilmorgan@ucsb.edu
 """
 import setuptools
 #import json
-#import os
-#this_directory = os.path.abspath(os.path.dirname(__file__))
-#with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-#    long_description = f.read()
+import os
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 NAME = "Tweezepy"
 VERSION = "1.0.4"
