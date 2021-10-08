1a050a05206f971f911781a0e643472a2bc1e6d7
Ian L Morgan <ilmorgan@ucsb.edu>
2021-10-08 10:31:46 -0700

    remove static path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -76,7 +76,7 @@ html_title = 'Tweezepy'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 html_theme_options = {
     "path_to_docs": "docs",
     "repository_url": "https://github.com/ianlmorgan/tweezepy",
