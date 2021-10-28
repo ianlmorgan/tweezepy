6bf74f75c625f166afac87d29de846c6e671ba9b
Ian L Morgan <ilmorgan@ucsb.edu>
2021-10-27 17:04:23 -0700

    updating documentation with myst_nb

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,8 @@ extensions = ["sphinx.ext.autodoc",
               "sphinx.ext.intersphinx",
               "sphinx.ext.napoleon",
               "sphinx.ext.mathjax",
-              "nbsphinx",
+              "myst_nb",
+              #"nbsphinx",
               ]
 
 #source_suffix = {
