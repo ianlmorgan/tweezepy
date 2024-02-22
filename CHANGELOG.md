d9fb439b066967226e0a199416f827a8e4440e19
Ian Morgan <ianmorgan92@gmail.com>
2024-02-22 16:45:22 -0500

    updates to pyproject.toml and quickstart tutorial

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+1.2.7
+* no longer excludes python 3.10
+* fixed bug that raised an error when fixing epsilon with tracking errors
+* moved installation information from setup.py to pyproject.toml
+* updated quickstart guide with examples for tracking error and fixed epsilon parameter
--- setup.py
+++ setup.py
@@ -3,45 +3,9 @@
 Created on Tue May  5 16:31:29 2020
 
 Author: Ian L. Morgan
-email: ilmorgan@ucsb.edu
+Description: Exists for back compatibility. 
+    All relevant information is now included in pyproject.toml
 """
 import setuptools
-import os
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
-# This call to setup() does all the work
-setuptools.setup(name="Tweezepy",
-                 version="1.2.6",
-                 author="Ian L. Morgan",
-                 author_email="ilmorgan@ucsb.edu",
-                 description="Single-molecule force spectroscopy calibration",
-                 url="https://github.com/ianlmorgan/tweezepy",
-                 license="GPLv3",
-                 packages=setuptools.find_packages(),
-                 include_package_data=True,
-                 python_requires = ">=3.6, !=3.10.*",
-                 long_description=long_description,
-                 long_description_content_type = 'text/markdown',
-                 install_requires = ["autograd",
-                                     "corner",
-                                     "emcee",
-                                     "matplotlib",
-                                     "numba",
-                                     "numpy>=1.15",
-                                     "scipy",
-                                     ],
-                 setup_requires = ["setuptools>=40.6.0"],
-                 tests_require=["unittest"],
-                 classifiers=["Intended Audience :: Science/Research",
-                              "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-                              "Operating System :: OS Independent",
-                              "Programming Language :: Python :: 3.6",
-                              "Programming Language :: Python :: 3.7",
-                              "Programming Language :: Python :: 3.8",
-                              "Programming Language :: Python :: 3.9",
-                              "Topic :: Scientific/Engineering",
-                              ],
-                 project_urls = {"Documentation" : "https://tweezepy.readthedocs.org"}
-                 ) 
+setuptools.setup()
